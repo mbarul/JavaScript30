9976e650f3d6b069823c8cbd77cb7427679b1366
Marek <marek.barul28@gmail.com>
2021-02-17 16:17:01 +0100

    finished, added clock function(JS) and media queries (CSS)

--- java.js
+++ java.js
@@ -1,12 +1,18 @@
 function clock() {
     const d = new Date();
-    console.log(d);
+    console.log(d)
+    const hours = (d.getHours() + 11) % 12 + 1;
+    const minutes = d.getMinutes();
+    const seconds = d.getSeconds();
 
+    const hour = hours * 30;
+    const minute = minutes * 6;
+    const second = seconds * 6;
+
+    document.querySelector('.hour').style.transform = `rotate(${hour}deg)`;
+    document.querySelector('.minute').style.transform = `rotate(${minute}deg)`;
+    document.querySelector('.second').style.transform = `rotate(${second}deg)`;
 }
+setInterval(clock, 1000);
 const d = new Date();
-console.log(d);
-const hours = d.getHours();
-console.log(hours);
-const minutes = d.getMinutes();
-const seconds = d.getSeconds();
-console.log(seconds);
+console.log(d)
