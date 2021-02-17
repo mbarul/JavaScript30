57c1abb418b5090ccefb3d0786629fb36c8c76d7
Marek <marek.barul28@gmail.com>
2021-02-17 15:19:40 +0100

    first lines in Javascript

---
+++ java.js
@@ -0,0 +1,12 @@
+function clock() {
+    const d = new Date();
+    console.log(d);
+
+}
+const d = new Date();
+console.log(d);
+const hours = d.getHours();
+console.log(hours);
+const minutes = d.getMinutes();
+const seconds = d.getSeconds();
+console.log(seconds);
