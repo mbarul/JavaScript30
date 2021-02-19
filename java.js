0383a183e07fc54d70b0577d23397bac6e5ae905
Marek <marek.barul28@gmail.com>
2021-02-19 10:12:42 +0100

    new function in javascript -change color of div background

--- java.js
+++ java.js
@@ -1,6 +1,15 @@
-function changed() {
+function changedBlur() {
     //getting image which one is to modify
     let img = document.getElementById('surf');
+    let slider = document.getElementById("rangeBlur").value;
     //indication of the style that we want to change and showing a value which will change blur of picture
-    img.style.filter = "blur(" + document.getElementById("rangeBlur").value + "px)";
+    img.style.filter = "blur(" + slider + "px)";
 }
+let colorInput = document.getElementById('color');
+let hexInput = document.getElementById('hex');
+
+colorInput.addEventListener('input', function changedColor() {
+    let color = colorInput.value;
+    hexInput.value = color;
+    document.getElementById("myDiv").style.backgroundColor = color;
+})
