67f6ad221c922edc85b1b0da0c248cbeb7954dcb
Marek <marek.barul28@gmail.com>
2021-02-19 10:25:52 +0100

    fix problem with blur

--- java.js
+++ java.js
@@ -1,10 +1,3 @@
-function changedBlur() {
-    //getting image which one is to modify
-    let img = document.getElementById('surf');
-    let slider = document.getElementById("rangeBlur").value;
-    //indication of the style that we want to change and showing a value which will change blur of picture
-    img.style.filter = "blur(" + slider + "px)";
-}
 let colorInput = document.getElementById('color');
 let hexInput = document.getElementById('hex');
 
@@ -13,3 +6,13 @@ colorInput.addEventListener('input', function changedColor() {
     hexInput.value = color;
     document.getElementById("myDiv").style.backgroundColor = color;
 })
+
+function changed() {
+    //getting image which one is to modify
+    let img = document.getElementById('surf');
+    let slider = document.getElementById("rangeBlur").value;
+    //indication of the style that we want to change and showing a value which will change blur of picture
+    img.style.filter = "blur(" + slider + "px)";
+}
+let slider = document.getElementById("rangeBlur").value;
+console.log(slider);
