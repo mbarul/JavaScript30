20742140061355c9618c8471971fe0a1189f67fd
Marek <marek.barul28@gmail.com>
2021-03-01 17:34:11 +0100

    finished work, added js and modifed css and html

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-console.log("HELLO!");
+const panels = document.querySelectorAll('.img');
+
+function toggleOpen() {
+    console.log("helo");
+    this.classList.toggle('open');
+}
+
+function toggleActive() {
+    console.log("jato");
+    this.classList.toggle('open-active');
+}
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
